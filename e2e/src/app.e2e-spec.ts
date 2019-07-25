f76e62a347c978b41daca08c8de8aaba34b74d72
Rolle <erpeetsalu@gmail.com>
2019-07-25 14:43:26 +0300

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to roland-movie-app!');
+    expect(page.getTitleText()).toEqual('Awesome Movie App');
   });
 
   afterEach(async () => {
